fdcbe1ee4bf98f1433328c7325cf8c51f7e35757
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-02-27 15:47:01 +0100

    ex-complete

--- java-festa-ferragnez/src/CheckGuest.java
+++ java-festa-ferragnez/src/CheckGuest.java
@@ -1,3 +1,35 @@
-public class CheckGuest {
 
+//scanner per leggere da tastiera/console
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+
+        String arrayGuest[] = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Bio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic", };
+
+        // scanner per leggere da tastiera/console
+        Scanner input = new Scanner(System.in);
+        System.out.println("Scrivi il nome che vuoi cercare nella lista:");
+        String userGuest = input.nextLine();
+
+        boolean guestFound = false;
+
+        for (int i = 0; i < arrayGuest.length; i++) {
+            // equals raffronta il valore delle stringhe
+            if (!guestFound && arrayGuest[i].equals(userGuest)) {
+                guestFound = true;
+                // se guestFound = true, interrompe il ciclo
+                break;
+            }
+        }
+
+        // stampo in console quanto risulta dal ciclo for
+        if (guestFound) {
+            System.out.println("Il nome " + userGuest + " è presente nella lista");
+        } else {
+            System.out.println("Il nome " + userGuest + " non è presente nella lista");
+        }
+        input.close();
+    }
 }
